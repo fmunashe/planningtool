31faea859f18a36673e12cab2a2ac511c1bca3b5
Farai <farai@flickerleap.com>
2026-01-15 22:46:04 +0200

    allow swagger

--- src/main/java/za/co/sabs/planningtool/config/SecurityConfig.java
+++ src/main/java/za/co/sabs/planningtool/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,6 +25,7 @@ import za.co.sabs.planningtool.service.security.JwtTokenProvider;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtTokenProvider tokenProvider;
